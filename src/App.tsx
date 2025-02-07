9b77467592e267ddfe3a3a53189dba2153259271
three4c <three4c@gmail.com>
2025-02-08 00:12:37 +0900

    get dom, get font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import { useEffect } from "react";
 import * as stylex from "@stylexjs/stylex";
+import "./reset.css";
 
 const styles = stylex.create({
   div: {
+    backdropFilter: "blur()12px",
+    backgroundColor: "rgba(255, 255, 255, 0.65)",
+    fontSize: "2em",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     width: 48,
     height: 48,
   },
 });
 
-const App: React.FC = () => {
+const DOM_ARRAY = ["INPUT", "TEXTAREA"];
+
+const App = () => {
+  const getFontSize = (element: Element) =>
+    window.getComputedStyle(element).fontSize;
+
   useEffect(() => {
     const onKeyDown = (e: KeyboardEvent) => {
-      console.log(e.key);
+      if (e.ctrlKey && e.key === "Enter") {
+        const activeElement = document.activeElement;
+        if (activeElement && DOM_ARRAY.includes(activeElement.tagName)) {
+          const fontSize = getFontSize(activeElement);
+          console.log(activeElement.tagName, fontSize);
+        }
+      }
     };
 
     window.addEventListener("keydown", onKeyDown);
     return () => window.removeEventListener("keydown", onKeyDown);
   }, []);
 
-  return <div {...stylex.props(styles.div)}>🦐</div>;
+  return (
+    <>
+      <div {...stylex.props(styles.div)}>🦐</div>
+      {/* <input type="text" /> */}
+      {/* <textarea /> */}
+    </>
+  );
 };
 
 export default App;
