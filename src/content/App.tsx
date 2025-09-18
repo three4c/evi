0ca7e648efb2cad9b69b7912a6266299ee86bb6d
ysjn <jun.yoyoyoshida@gmail.com>
2025-09-18 09:40:27 +0900

    feat: Add visual / yank / paste (v / y / p / P) (#4)
    * feat: visual / yank / paste
    
    * feat: Add visual left(h) & visual right(l) movements
    
    * fix: leave j/k for later
    
    * fix: Add p/P in visual mode, moved normal o/O, p/P inside mode condition
    
    * fix: fix selection movement on edge
    
    * fix: improve conditions, define total text length
    
    * fix: use smarter method for replacing selection
    
    * chore: remove dist/

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -16,6 +16,14 @@ const App: React.FC = () => {
     start: 0,
     end: 0,
   });
+  const originalPos = useRef<
+    | {
+        start: number;
+        end: number;
+        currentLine: number;
+      }
+    | undefined
+  >(undefined);
   const [shortcuts, setShortcuts] = useState<Record<string, ShortcutConfig>>(
     {},
   );
@@ -55,11 +63,12 @@ const App: React.FC = () => {
     };
   };
 
-  const startVim = (
+  const startVim = async (
     element: HTMLInputElement | HTMLTextAreaElement,
     e: KeyboardEvent,
   ) => {
     let { start, end } = pos.current;
+    const { length } = element.value;
 
     if (mode.current === "normal") {
       start = element.selectionStart || 0;
@@ -68,69 +77,166 @@ const App: React.FC = () => {
 
     const { lines, charCount, currentLine, col } = getLines(element, start);
 
-    if (e.key === "h" && col) {
-      start--;
-      end = start + 1;
-    }
-
-    if (e.key === "l" && col !== lines[currentLine].length - 1) {
-      start++;
-      end = start + 1;
-    }
-
-    if (e.key === "j" && currentLine + 1 < lines.length) {
-      const nextLineLength = lines[currentLine + 1].length;
-      const next = charCount + lines[currentLine].length + 1;
-
-      if (nextLineLength) {
-        const overCharCount =
-          col >= nextLineLength ? col - nextLineLength + 1 : 0;
-        start = next + col - overCharCount;
-      } else {
-        start = next;
+    if (mode.current === "normal") {
+      if (e.key === "h" && col) {
+        start--;
+        end = start + 1;
       }
-      end = start + 1;
-    }
 
-    if (e.key === "k" && currentLine > 0) {
-      const prevLineLength = lines[currentLine - 1].length;
-      const prev = charCount - (lines[currentLine - 1].length + 1);
-
-      if (prevLineLength) {
-        const overCharCount =
-          col >= prevLineLength ? col - prevLineLength + 1 : 0;
-        start = prev + col - overCharCount;
-      } else {
-        start = prev;
+      if (e.key === "l" && col !== lines[currentLine].length - 1) {
+        start++;
+        end = start + 1;
       }
-      end = start + 1;
+
+      if (e.key === "j" && currentLine + 1 < lines.length) {
+        const nextLineLength = lines[currentLine + 1].length;
+        const next = charCount + lines[currentLine].length + 1;
+
+        if (nextLineLength) {
+          const overCharCount =
+            col >= nextLineLength ? col - nextLineLength + 1 : 0;
+          start = next + col - overCharCount;
+        } else {
+          start = next;
+        }
+        end = start + 1;
+      }
+
+      if (e.key === "k" && currentLine > 0) {
+        const prevLineLength = lines[currentLine - 1].length;
+        const prev = charCount - (lines[currentLine - 1].length + 1);
+
+        if (prevLineLength) {
+          const overCharCount =
+            col >= prevLineLength ? col - prevLineLength + 1 : 0;
+          start = prev + col - overCharCount;
+        } else {
+          start = prev;
+        }
+        end = start + 1;
+      }
+
+      if (e.key === "o" && element.tagName === "TEXTAREA") {
+        const nextBreak = element.value.indexOf("\n", start);
+        start = nextBreak === -1 ? length : nextBreak;
+        element.value = [
+          element.value.slice(0, start),
+          element.value.slice(start, length),
+        ].join("\n");
+        start = end = start + 1;
+        mode.current = "insert";
+      }
+
+      if (e.key === "O" && element.tagName === "TEXTAREA") {
+        const prevBreak = element.value.lastIndexOf("\n", start - 1);
+        start = prevBreak === -1 ? 0 : prevBreak;
+        element.value = [
+          element.value.slice(0, start),
+          element.value.slice(start, length),
+        ].join("\n");
+        start = end = start + (start ? 1 : 0);
+        mode.current = "insert";
+      }
+
+      if (e.key === "p") {
+        const text = await navigator.clipboard.readText();
+        if (text) {
+          const pos = lines[currentLine].length === 0 ? start : end;
+          element.value = [
+            element.value.slice(0, pos),
+            element.value.slice(pos, length),
+          ].join(text);
+          start = start + text.length;
+          end = start + 1;
+        }
+      }
+
+      if (e.key === "P") {
+        const text = await navigator.clipboard.readText();
+        if (text) {
+          element.value = [
+            element.value.slice(0, start),
+            element.value.slice(start, length),
+          ].join(text);
+          start = start + text.length - 1;
+          end = start + 1;
+        }
+      }
+
+      if (lines[currentLine].length && col === lines[currentLine].length) {
+        start = start - 1;
+        end = start + 1;
+      }
     }
 
-    if (e.key === "o" && element.tagName === "TEXTAREA") {
-      const nextBreak = element.value.indexOf("\n", start);
-      start = nextBreak === -1 ? element.value.length : nextBreak;
-      element.value = [
-        element.value.slice(0, start),
-        element.value.slice(start, element.value.length),
-      ].join("\n");
-      start = end = start + 1;
-      mode.current = "insert";
-    }
+    if (mode.current === "visual" && originalPos.current) {
+      const { start: oStart, end: oEnd } = originalPos.current;
 
-    if (e.key === "O" && element.tagName === "TEXTAREA") {
-      const prevBreak = element.value.lastIndexOf("\n", start - 1);
-      start = prevBreak === -1 ? 0 : prevBreak;
-      element.value = [
-        element.value.slice(0, start),
-        element.value.slice(start, element.value.length),
-      ].join("\n");
-      start = end = start + (start ? 1 : 0);
-      mode.current = "insert";
-    }
+      const isSingleChar = end - start === 1;
+      const shouldMoveStart = isSingleChar ? start <= oStart : start < oStart;
+      const shouldMoveEnd = isSingleChar ? end >= oEnd : end > oEnd;
+      const atLeftEdge = start === 0 && oStart === 0;
+      const atRightEdge = end === length && oEnd === length;
 
-    if (lines[currentLine].length && col === lines[currentLine].length) {
-      start = start - 1;
-      end = start + 1;
+      if (e.key === "h") {
+        if (start > 0) {
+          if (shouldMoveStart) {
+            start--;
+          } else {
+            end--;
+          }
+        } else if (atLeftEdge && end !== 1 && !isSingleChar) {
+          end--;
+        }
+      }
+
+      if (e.key === "l") {
+        if (end < length) {
+          if (shouldMoveEnd) {
+            end++;
+          } else {
+            start++;
+          }
+        } else if (atRightEdge && start !== length - 1 && !isSingleChar) {
+          start++;
+        }
+      }
+
+      // if (e.key === "j" && currentLine + 1 < lines.length) {
+      //   if (currentLine === oCurrentLine || currentLine > oCurrentLine) {
+      //     end = end + lines[currentLine].length + 1;
+      //   } else {
+      //     start = start + lines[currentLine].length + 1;
+      //   }
+      // }
+      //
+      // if (e.key === "k" && currentLine > 0) {
+      //   if (currentLine === oCurrentLine || currentLine < oCurrentLine) {
+      //     start = start - lines[currentLine].length - 1;
+      //   } else {
+      //     end = end - lines[currentLine].length - 1;
+      //   }
+      // }
+
+      if (e.key === "p" || e.key === "P") {
+        const text = await navigator.clipboard.readText();
+        if (text) {
+          element.setRangeText(text);
+          start = start + text.length - 1;
+          end = start + 1;
+          mode.current = "normal";
+        }
+      }
+
+      if (e.key === "y") {
+        const text = window.getSelection()?.toString();
+        if (text) {
+          navigator.clipboard.writeText(text);
+          mode.current = "normal";
+          start = oStart;
+          end = oEnd;
+        }
+      }
     }
 
     if (e.key === "i") {
@@ -146,7 +252,15 @@ const App: React.FC = () => {
     }
 
     if (e.key === "v") {
-      mode.current = "visual";
+      if (mode.current === "normal") {
+        mode.current = "visual";
+        originalPos.current = { start, end, currentLine };
+      } else {
+        mode.current = "normal";
+        start = originalPos.current?.start || start;
+        end = originalPos.current?.end || end;
+        originalPos.current = undefined;
+      }
     }
 
     pos.current = { start, end };
