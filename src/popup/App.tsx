c55034c8cfe7ea3b84208264ef8d8bd73bc2d36a
jyoshida <jyoshida@lycorp.co.jp>
2025-09-18 10:41:01 +0900

    fix: add feedback for option button

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -10,21 +10,28 @@ const styles = stylex.create({
     alignItems: "center",
     justifyContent: "center",
     padding: "8px 16px",
+    userSelect: "none",
   },
   p: {
     color: "#fff",
     fontWeight: "600",
     fontFamily: "'Rubik', serif",
     fontSize: 16,
+    pointerEvents: "none",
   },
   span: {
     marginRight: 8,
+    pointerEvents: "none",
   },
   settings: {
     marginLeft: 8,
     padding: 0,
     background: "transparent",
     border: "none",
+    ":hover": {
+      cursor: "pointer",
+      filter: "brightness(0.8)",
+    },
   },
   dev: {
     display: "flex",
