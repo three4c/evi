f418e948fe394739f49e59d1e755216d0952f023
jyoshida <jyoshida@lycorp.co.jp>
2025-09-19 21:57:47 +0900

    fix: add workaround for delete (s/S)

--- src/utils/insertText.ts
+++ src/utils/insertText.ts
@@ -5,8 +5,16 @@ export const insertText = (
   value: string,
 ) => {
   element.setSelectionRange(start, end);
-  // ブラウザのテキスト変更履歴にのせるため、execCommandを使用
-  document.execCommand("insertText", false, value);
+  if (value === "" && element.value.length === end - start) {
+    // 文字数が0になる場合は、操作不能に陥る不具合があるため
+    // execCommandで一度スペースを挿入してから削除する
+    document.execCommand("insertText", false, " ");
+    element.setSelectionRange(0, 1);
+    document.execCommand("delete");
+  } else {
+    // ブラウザのテキスト変更履歴にのせるため、execCommandを使用
+    document.execCommand("insertText", false, value);
+  }
   // ユーザーが書き換えた時と同等のイベントを発火
   element.dispatchEvent(new InputEvent("input", { bubbles: true }));
 };
