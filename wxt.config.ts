7a8d86a8f436ccc53fc33bf9cb578748812c657d
three4c <three4c@gmail.com>
2025-09-21 01:29:56 +0900

    add: content

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   srcDir: "src",
+  manifest: {
+    permissions: ["storage", "clipboardRead"],
+  },
 });
