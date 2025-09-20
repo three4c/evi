ba415fec0c477993aa36866c812184fffe1596f5
jyoshida <jyoshida@lycorp.co.jp>
2025-09-20 22:07:14 +0900

    fix: create detectModifierKey.ts

---
+++ src/utils/detectModifierKey.ts
@@ -0,0 +1,9 @@
+const detectModifierKey = (e: KeyboardEvent) => {
+  let keys = [];
+  if (e.ctrlKey) keys.push("ctrl");
+  if (e.altKey) keys.push("alt");
+  keys.push(e.key);
+  return keys.join("+");
+};
+
+export { detectModifierKey };
