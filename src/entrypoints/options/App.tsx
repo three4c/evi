19033e91d483d4fecab5f51afd8bb1ec9ee72a7d
three4c <three4c@gmail.com>
2025-09-25 13:34:26 +0900

    fix: test

--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { DEFAULT_KEYMAPS } from "@/keymaps";
 import type { Keymap, Keymaps, MODE_TYPE } from "@/utils";
 import {
