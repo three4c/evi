e8275a5472baaa2d3b4c34b84d2edadf9d0c6a94
jyoshida <jyoshida@lycorp.co.jp>
2025-09-25 22:10:07 +0900

    fix: fix not being able to enter normal mode from end of whole text

--- src/commands/insert.ts
+++ src/commands/insert.ts
@@ -1,8 +1,14 @@
 import type { Command } from "@/utils";
 
 export const INSERT_COMMANDS: Record<string, Command> = {
-  enter_normal_mode: ({ element, start, end }) => {
+  enter_normal_mode: ({ element, start, end, length, lines, currentLine }) => {
     start = element.selectionStart || 0;
+    if (
+      start === length ||
+      (lines[currentLine].length && element.value.charAt(start) === "\n")
+    ) {
+      start--;
+    }
     end = start + 1;
     element.setSelectionRange(start, end);
     return { start, end, mode: "normal" };
