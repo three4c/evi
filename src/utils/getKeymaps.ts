0c39a52cd4b64fa2fd4548796324611747c69191
three4c <three4c@gmail.com>
2025-09-27 21:55:00 +0900

    fix: getKeymaps

--- src/utils/getKeymaps.ts
+++ src/utils/getKeymaps.ts
@@ -1,14 +1,17 @@
 import { DEFAULT_KEYMAPS } from "@/keymaps";
 import { loadKeymaps } from "@/utils";
 
-export const getKeymaps = async () => {
-  const savedKeymaps = await loadKeymaps();
-  const cachedKeymaps = {
-    common: { ...DEFAULT_KEYMAPS.common, ...savedKeymaps.common },
-    insert: { ...DEFAULT_KEYMAPS.insert, ...savedKeymaps.insert },
-    normal: { ...DEFAULT_KEYMAPS.normal, ...savedKeymaps.normal },
-    visual: { ...DEFAULT_KEYMAPS.visual, ...savedKeymaps.visual },
-  };
+let cachedKeymaps: Keymaps | null = null;
 
+export const getKeymaps = async () => {
+  if (!cachedKeymaps) {
+    const savedKeymaps = await loadKeymaps();
+    cachedKeymaps = {
+      common: { ...DEFAULT_KEYMAPS.common, ...savedKeymaps.common },
+      insert: { ...DEFAULT_KEYMAPS.insert, ...savedKeymaps.insert },
+      normal: { ...DEFAULT_KEYMAPS.normal, ...savedKeymaps.normal },
+      visual: { ...DEFAULT_KEYMAPS.visual, ...savedKeymaps.visual },
+    };
+  }
   return cachedKeymaps;
 };
