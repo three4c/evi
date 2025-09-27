259469ddeb8d7d292d00526def34a75e3dc5b6b5
jyoshida <jyoshida@lycorp.co.jp>
2025-09-28 01:25:01 +0900

    fix: always return to insert mode on blur

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -4,12 +4,14 @@ let initComplete = false;
 let mode: MODE_TYPE = "insert";
 let pos: Positions = { start: 0, end: 0, oStart: 0, oEnd: 0, oCurrentLine: 0 };
 
+const enterInsertMode = () => (mode = "insert");
 const keydown = async (e: KeyboardEvent) =>
   ({ mode, pos } = await handleKeyDown(e, { mode, pos }));
 
 export const initVim = () => {
   if (initComplete) return;
   window.addEventListener("keydown", keydown);
+  document.addEventListener("focusout", enterInsertMode);
   initComplete = true;
 };
 
