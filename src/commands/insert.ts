c9fe9ee05388278bd1772ae547175b6af26b1086
jyoshida <jyoshida@lycorp.co.jp>
2025-10-06 19:30:30 +0900

    fix: minor fix condition

--- src/commands/insert.ts
+++ src/commands/insert.ts
@@ -4,7 +4,7 @@ export const INSERT_COMMANDS: Record<string, Command> = {
   enter_normal_mode: ({ element, start, end, length }) => {
     start = element.selectionStart || 0;
     if (
-      (start !== 0 && element.value[start] === "\n") ||
+      (start > 0 && element.value[start] === "\n") ||
       (start === length && !element.value.endsWith("\n"))
     ) {
       start--;
