8b7498b15c7ec18ba3be68c72745c463f3753be8
みよ <three4c@gmail.com>
2025-10-08 23:04:52 +0900

    feat: Add reset badge (#31)
    * add: reset badge
    
    * fix: modeMap
    
    * fix: MODE_TYPE→ModeType
    
    * fix: type
    
    * fix: modeMap

--- src/utils/modeMap.ts
+++
@@ -1,7 +0,0 @@
-export const modeMap: Record<
-  "normal" | "visual",
-  { text: "NOR" | "VIS"; color: [number, number, number, number] }
-> = {
-  normal: { text: "NOR", color: [100, 149, 237, 255] },
-  visual: { text: "VIS", color: [255, 140, 0, 255] },
-};
