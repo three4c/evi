c73481f7e5428925dae6cb242a6e7865712a8afc
three4c <three4c@gmail.com>
2025-10-28 01:27:53 +0900

    fix: tabId

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -38,6 +38,7 @@ export const onKeymapsChangedMessaged = () => {
           if (tab.id) {
             chrome.tabs.sendMessage(tab.id, {
               keymaps: changes.keymaps.newValue,
+              tabId: tab.id,
             });
           }
         }
@@ -69,8 +70,8 @@ export const sendMessage = <T>(args: T) => {
 
 export const onMessage = <T>(callback: (args: T, tabId: number) => void) => {
   chrome.runtime.onMessage.addListener((message, sender) => {
-    if (sender.tab?.id) {
-      callback(message.args, sender.tab.id);
+    if (sender.tab?.id || message.tabId) {
+      callback(message.args, sender.tab?.id || message.tabId);
     }
   });
 };
